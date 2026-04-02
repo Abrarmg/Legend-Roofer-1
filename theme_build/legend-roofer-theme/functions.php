ead768dccff8260978d0e39e9092785a404022e2
Macbook Pro 2019 <macbookpro@macbooks-MacBook-Pro.local>
2026-04-02 15:15:13 +0500

    chore: upload compiled WordPress theme package

---
+++ theme_build/legend-roofer-theme/functions.php
@@ -0,0 +1,5 @@
+<?php
+function legend_roofer_setup() {
+    add_theme_support( 'title-tag' );
+}
+add_action( 'after_setup_theme', 'legend_roofer_setup' );
---
+++ theme_build/legend-roofer-theme/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+  <head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title><?php bloginfo( 'name' ); ?></title>
+    <?php wp_head(); ?>
+    <script type="module" crossorigin src="<?php echo get_template_directory_uri(); ?>/assets/index-BbTnnZLM.js"></script>
+    <link rel="stylesheet" crossorigin href="<?php echo get_template_directory_uri(); ?>/assets/index-Bxy_ELMP.css">
+  </head>
+  <body <?php body_class(); ?>>
+    <?php wp_body_open(); ?>
+    <div id="root"></div>
+    <?php wp_footer(); ?>
+  </body>
+</html>
